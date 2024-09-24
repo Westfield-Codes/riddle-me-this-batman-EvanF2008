2c2535482e238468a2ce4e5fc2383eacdb048f3c
EvanF2008 <evan118544@student-schoolsofwestfield.org>
2024-09-24 13:53:48 +0000

    new riddlesss

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bellRiddle(){
   var answer = prompt("What word in the dictonary in spelt incorrectly?");
-  if (answer == "incorrectly") alert("Correct!");
+  if (answer == "incorrectly") alert("YES!");
   else alert("WRONG, try again sonny.");
 }
