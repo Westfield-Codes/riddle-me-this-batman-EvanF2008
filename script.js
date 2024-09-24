b75f2d330d0f07bbb2f735d3f8cf40faac3a84fd
EvanF2008 <evan118544@student-schoolsofwestfield.org>
2024-09-24 13:49:37 +0000

    new riddle

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function bellRiddle(){
-  var answer = prompt("Why is an orange like a bell? Because they both must be ...");
-  if (answer == "peeled" || answer == "pealed") alert("Correct!");
-  else alert("Wrong, Batman! Watch the video, and try again.");
+  var answer = prompt("What word in the dictonary in spelt incorrectly?");
+  if (answer == "incorrectly") alert("Correct!");
+  else alert("WRONG, try again sonny.");
 }
